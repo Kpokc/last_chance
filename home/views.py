db817b4d688b568777619eff39baadcf63967b32
Kpokc <pavmakarovs@gmail.com>
2021-11-05 12:40:09 +0000

    Added - home page

---
+++ home/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+
+def index(request):
+    """ A view to return the index page """
+    #return HttpResponse("HElllllllooooooo")
+    return render(request, 'home/index.html')
